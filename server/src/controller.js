5bb175af46b685a6d027997d81b6bf55657a0e8a
Md Shohanuzzaman <shohaniu8@gmail.com>
2021-03-21 23:52:04 +0600

    Garage and controller get connected

--- server/src/controller.js
+++ server/src/controller.js
@@ -10,8 +10,32 @@ client.on('connect', ()=>{
 })
 
 client.on('message', (topic,message)=>{
-    if(topic == 'garage/connected'){
-        connected = (message.toString() == 'ture')
+    // if(topic == 'garage/connected'){
+    //     connected = (message.toString() == 'ture')
+    // }
+    
+    switch(topic){
+        case 'garage/connected':
+            return handleGarageConnected(message);
+        case 'garage/state':
+            return handleGarageState(message);
     }
 
+    console.log('No handler for topic %s', topic);
+
 })
+
+
+
+function handleGarageConnected(message){
+    console.log('garage connected status %s', message)
+    connected = (message.toString() == 'true');
+}
+
+
+function handleGarageState(message){
+    garageState = message;
+    console.log('garage state update to %s', message);
+}
+
+
